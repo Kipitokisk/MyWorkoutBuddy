28ec0c942aa59102fde5d971dcb7fcbd55ef364b
Kipitokisk <victor.revenoc.00@gmail.com>
2025-06-01 15:15:37 +0300

    Add repository for entities

---
+++ src/main/java/com/example/MyWorkoutBuddy/repository/ExerciseRepository.java
@@ -0,0 +1,13 @@
+package com.example.MyWorkoutBuddy.repository;
+
+import com.example.MyWorkoutBuddy.model.Exercise;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ExerciseRepository extends JpaRepository<Exercise, Long> {
+    List<Exercise> findByFavoriteTrueAndUserId(Long userId);
+    List<Exercise> findByUserId(Long userId);
+}
